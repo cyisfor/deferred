d81fd80baa85ddf128045a28466089efa3a74de4
Cy <whatever>
2013-10-22 22:55:51 -0700

    Still trying to get graph working right.

--- deferred/graph/__init__.py
+++ deferred/graph/__init__.py
@@ -1,7 +1,9 @@
 #import locator
+from graph import Node,graph
 import deferred
-from . import graph
 from deferred._defer import passthru
+
+from functools import reduce
 
 import traceback
 import sys
@@ -14,34 +16,91 @@ def newacbs(self, callback, errback=None,
                      errbackArgs=None, errbackKeywords=None):
     for cb in (callback,errback):
         if cb:
-            print('adding',cb)
+            if cb is passthru: continue
             try:
                 raise ZeroDivisionError
             except ZeroDivisionError:
-                contexts[cb] = sys.exc_info()[2].tb_frame.f_back.f_back
+                stage = Node((id(self),len(self.callbacks)),"derp")
+                contexts[stage] = list(reversed(traceback.extract_stack()[:-2]))
     return oldacbs(self,callback,errback,callbackArgs,callbackKeywords,errbackArgs,errbackKeywords)
 if oldacbs is not newacbs:
     deferred.Deferred.addCallbacks = newacbs
 
-def graphtree(d,graph):
-    def doCTX(cb,ctx):
-        graph.function(cb,ctx)
-        while ctx.f_back:
-            graph.back(ctx,ctx.f_back)
-            ctx = ctx.f_back
-    for stage,thing in enumerate(d.callbacks):
-        cb = thing[0][0]
-        if stage>0:
-            graph.bump(stage-1,stage)
-        eb = thing[1][0]
-        def derp(cb):
-            ctx = contexts.get(cb)
+oldcallback = deferred.Deferred.callback
+
+def backtraceShower(graph):
+    def doCTX(cb,tb):
+        nonlocal graph
+        lastFrame = None
+        firstFrame = None
+        for frame in tb:
+            frame = Node('{}:{}:{}'.format(*(frame[:3])),'{}:{}'.format(frame[2],frame[1]))
+            if lastFrame:
+                graph.back(lastFrame,frame)
+            lastFrame = frame
+            if not firstFrame:
+                firstFrame = frame
+        if firstFrame:
+            graph.function(cb,firstFrame)
+    return doCTX
+
+deferred.Deferred.graphed = False
+
+def tree(d,graph):
+    if d.graphed: return
+    d.graphed = True
+    if not d.callbacks: return
+    doCTX = backtraceShower(graph)
+    buddies = []
+    stages = []
+    with graph.subgraph('deferred') as dgraph:
+        if not d.callbacks: return
+        lastStage = None
+        for stage in range(len(d.callbacks)):            
+            stage = Node((id(d),stage),"stage {}".format(stage))
+            dgraph.defers(d,stage)
+            dgraph.nodes.add(stage)
+            stages.append(stage)
+            ctx = contexts.get(stage)
             if ctx:
-                contexts.pop(cb)
-                doCTX(cb,ctx)
-        graph.callback(stage,cb)
-        graph.errback(stage,eb)
-        derp(eb)
-        derp(cb)
-    for cb,ctx in contexts.items():
-        doCTX(cb,ctx)
+                contexts.pop(stage)
+                doCTX(stage,ctx)
+            if lastStage is not None:
+                dgraph.nextStage(stage,lastStage)
+            lastStage = stage
+    def process(side,errback=False):
+        nonlocal graph
+        if errback:
+            name = 'errbacks'
+        else:
+            name = 'callbacks'
+        side = tuple(tuple(i) for i in side)
+        lastStage = None
+        firstStage = None
+        for stage,thing in enumerate(side):            
+            if hasattr(thing[0],'__self__') and isinstance(thing[0].__self__,deferred.Deferred):
+                thing = thing[0].__self__
+                isDeferred = True
+            else:
+                isDeferred = False
+            stage = stages[stage]
+            with graph.subgraph(name) as subgraph:
+                if errback:
+                    subgraph.errback(stage,thing)
+                else:
+                    print('callback',stage,thing)
+                    subgraph.callback(stage,thing)
+                if isDeferred:
+                    subgraph.chain(d,thing)
+                    buddies.append(thing)
+    process((thing[0] for thing in d.callbacks),False)
+    process((thing[1] for thing in d.callbacks),True)
+    for buddy in buddies:
+        tree(buddy,graph)
+
+def extra(graph):
+    if len(contexts)==0: return
+    doCTX = backtraceShower(graph)
+    with graph.subgraph("extra"):
+        for cb,ctx in contexts.items():
+            doCTX(cb,ctx)
--- deferred/graph/graph.py
+++ deferred/graph/graph.py
@@ -1,22 +1,40 @@
 import deferred
 import random
 
-from deferred._defer import passthru
+from contextlib import contextmanager
+from itertools import count
+
+from deferred._defer import passthru,Deferred
+
+# XXX: this is so roundabout
+
 
 def nameOf(node):
-    if hasattr(node,'f_code'):
-        return node.f_code.co_name
+    if hasattr(node,'f_name'):
+        return node.f_name
     elif hasattr(node,'__name__'):
         name = node.__name__
         if name == '<lambda>':
             co = node.__code__
             name = '<lambda '+co.co_filename+':'+str(co.co_firstlineno)+'>'
         return name
+    elif hasattr(node,'f_code'):
+        code = node.f_code
+        label = '{}:{}'.format(code.co_filename,code.co_firstlineno)
+        return label
     elif isinstance(node,int):
         return 'stage '+str(node)
+    elif isinstance(node,str):
+        return node
+    elif isinstance(node,tuple):
+        if node[0] is passthru: return
+        return nameOf(node[0])
+    elif isinstance(node,Deferred):
+        return str(node)
+    elif hasattr(node,'name'):
+        return node.name
     else:
-        print(node,type(node),dir(node))
-        raise RuntimeError
+        raise RuntimeError(node,type(node),dir(node))
 
 def info(type):
     if type == 'errback':
@@ -28,18 +46,24 @@ def info(type):
     elif type== 'next':
         return '[color="0.0,0.0,0.0",label="next",w=2.0,len=1]'
     elif type == 'callback':
-        return '[color="0.6,1.0,1.0",w=1.0,len=2]'
+        return '[color="0.6,1.0,1.0",w=1.0,len=2,label="callback"]'
     elif type == 'same':
-        return '[color="0.0,0.0,0.5",w=0.0]'
+        return '[color="0.0,0.0,0.5",w=1.0,label="same"]'
+    elif type == 'defers':
+        return '[color="0.5,0.0,0.5",w=1.0,label="defers"]'
+    elif type == 'chain':
+        return '[color="0.5,0.5,0.5",w=1.0,label="chains"]'
     else:
         raise RuntimeError("What is",type)
 
 from functools import total_ordering
 @total_ordering
 class Node:
-    def __init__(self,id,label):
+    def __init__(self,id,label=None):
         self.id = id
         self.name = str(id)
+        if label is None:
+            label = self.name
         self.label = label
     def __hash__(self):
         return hash(self.id)
@@ -50,18 +74,52 @@ class Node:
     def __repr__(self):
         return repr(self.id)
 
+tab = ' '
+
+def defid(d):
+    return '<Deferred '+hex(id(d))+'>'
+
+colors = {
+    'callbacks': 'seagreen1',
+    'deferred': 'palegoldenrod',
+    'errbacks': 'red',
+    'extra': 'mediumpurple',
+}
+
+counter = count(0)
+
 class Graph:
-    def __init__(self,dest):
+    buffering = False
+    gotSub = False
+    level = 0
+    def __init__(self,dest,root=None,sub=False):
+        if root is None:
+            self.root = self
+        else:
+            self.root = root
         self.dest = dest
-        dest.write('digraph {\n')
         self.nodes = set()
-        self.donedid = set()
+        self.relationships = set()
         self.counter = 0
+        self.sub = sub
+        self.subs = []
+        self.headers = []
+    def write(self,what):
+        if self.level:
+            self.dest.write(tab*self.level)
+        self.dest.write(what)
     def hashh(self,a):
         if isinstance(a,Node): return a
-        return Node(hash(a),nameOf(a))
-    def bump(self,a,b):
-        self.update(a,b,'next')
+        try:
+            return Node(hash(a),nameOf(a))
+        except TypeError:
+            return Node(id(a),nameOf(a))
+    def defers(self,d,first):
+        self.update(defid(d),first,'defers')
+    def nextStage(self,last,current):
+        self.update(last,current,'next')
+    def chain(self,d,b):
+        self.update(defid(d),b,'chain')
     def update(self,a,b,type):
         assert a is not None
         assert b is not None
@@ -69,11 +127,22 @@ class Graph:
         if b is passthru: return
         a = self.hashh(a)
         b = self.hashh(b)
-        if (a,b) in self.donedid: return
-        self.donedid.add((a,b))
-        self.dest.write('"'+a.name+'" -> "'+b.name+'" '+info(type) + '\n')
-        self.nodes.add(a)
-        self.nodes.add(b)
+        if not a.label or not b.label: return
+        if (a,b,type) in self.relationships: return
+        self.root.relationships.add((a,b,type))
+        if self.root is not self:
+            self.nodes.add(a)
+            self.nodes.add(b)
+    def header(self,a):
+        self.headers.append(a)
+    @contextmanager
+    def subgraph(self,name=''):
+        print('subgraph',self.sub,name)
+        sub = Graph(self,self.root,sub=name)
+        sub.header("color={};\n".format(colors.get(name,'blue')))
+        sub.header("label="+name+";\n")
+        self.subs.append(sub)
+        yield sub
     def callback(self,stage,cb):
         self.update(stage,cb,'callback')
     def errback(self,stage,eb):
@@ -82,14 +151,35 @@ class Graph:
         self.update(cb,ctx,'function')
     def back(self,low,high):
         self.update(low,high,'back')
+    donedid = None
     def finish(self):
+        if self.root.donedid is None:
+            self.root.donedid = set()
+        dest = self.dest
+        if self.sub:
+            self.write("subgraph cluster{}_{} ".format(self.sub,next(counter))+ '{ \n')
+        else:
+            self.write('digraph {\n')
+        self.level += 1
         for node in self.nodes:
-            self.dest.write('"'+node.name+'" [label="'+node.label+'"]'+'\n')
-        self.dest.write('}\n')
+            if not node in self.root.donedid:
+                self.write('"'+node.name+'" [label="'+node.label+'"]'+'\n')
+                self.root.donedid.add(node)
+        for sub in self.subs:
+            print(self.level)
+            sub.level = self.level
+            sub.finish()
+        for header in self.headers:
+            self.write(header)
+        for a,b,type in self.relationships:
+            self.write('"{}" -> "{}" {}\n'.format(a,b,info(type)))
+        print('-')
+        self.level -= 1
+        self.write('}\n')
 
 from contextlib import contextmanager
 @contextmanager
-def withit(path):
+def graph(path):
     with open(path,'wt') as out:
         g = None
         try:
@@ -99,7 +189,3 @@ def withit(path):
             if g:
                 g.finish()
 
-import sys
-module=sys.modules[__name__]
-withit.moduleboo = module
-sys.modules[__name__] = withit
